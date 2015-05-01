a1bd0a91040beb9c3c3deec35d87127ae361c1b8
Amit Dubey <amit.dubey2219@gmail.com>
2015-05-01 13:39:56 -0700

    create tenant service and dao

---
+++ src/main/java/app/controllers/UserController.java
@@ -0,0 +1,24 @@
+package app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.services.TenantServiceIfc;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	TenantServiceIfc service;
+
+	@RequestMapping(method = RequestMethod.POST, value = "/createTenant/{userId}/{sdlcType}")
+	public @ResponseBody boolean createTenant(@PathVariable String userId,
+			@PathVariable String sdlcType) {
+		return service.createTenant(userId, sdlcType);
+	}
+
+}
